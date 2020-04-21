a55dd9c498975384692b9a806d4f34e064ac5a6a
bprasa <b_venkata_prasad@gap.com>
2020-04-21 16:35:34 +0530

    Initial Setup for Week1

---
+++ Week1/src/main/java/com/gap/learnners/week1/SubArrayCountWithGivenTotal.java
@@ -0,0 +1,20 @@
+package com.gap.learnners.week1;
+
+public class SubArrayCountWithGivenTotal {
+
+    public static void main(String[] args) {
+
+        int[] array= {1,2,3,4,5};
+
+        System.out.println("sub arrays for given sum 8 is : "+countOfSubArrays(array,8));
+
+    }
+
+    static int countOfSubArrays(int[] array, int sum){
+        int count= 0 ;
+
+
+
+        return count;
+    }
+}
